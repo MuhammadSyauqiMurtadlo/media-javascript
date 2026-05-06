f266f8dcaf357e1ba16093f1b0790dea9b79f293
Syauqi <sauki084@gmail.com>
2026-05-06 14:11:00 +0700

    add library EJS

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs"); //embeded javascript
+app.set("views", "halaman");
 
-console.log(5 ** 4);
+app.get("/dashboard", (req, res) => {
+  res.render("Beranda");
+});
+
+// console.log(5 ** 4);
 app.listen(7500, () => console.log("Server is running on port 7500"));
